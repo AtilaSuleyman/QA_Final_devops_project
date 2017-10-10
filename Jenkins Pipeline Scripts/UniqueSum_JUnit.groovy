67884ee2b09f7c9e60eed446728f7bdff9f28696
scott1541 <scott3933@gmail.com>
2017-10-10 17:35:05 +0100

    Update script

--- Jenkins Pipeline Scripts/UniqueSum_JUnit.groovy
+++ Jenkins Pipeline Scripts/UniqueSum_JUnit.groovy
@@ -2,7 +2,7 @@ node {
    def mvnHome
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
-      git 'https://github.com/scott1541/UniqueSum.git'
+      git 'https://bwoodhouse322@bitbucket.org/devops322/uniquesum.git'
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.           
@@ -22,6 +22,6 @@ node {
    }
    
     stage('Publish') {
-     nexusArtifactUploader artifacts: [[artifactId: 'SpecV2', classifier: '', file: '/var/jenkins_home/workspace/DevOpsBuilds/target/Dice-Game-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'NexusAdmin', groupId: 'SpecOps', nexusUrl: 'ec2-34-250-175-227.eu-west-1.compute.amazonaws.com:50000/nexus', nexusVersion: 'nexus2', protocol: 'http', repository: 'releases', version: env.BUILD_NUMBER
+     nexusArtifactUploader artifacts: [[artifactId: 'SpecV2', classifier: '', file: '/var/jenkins_home/workspace/DevOpsBuilds/target/Dice-Game-1.0-SNAPSHOT.jar', type: 'jar']], credentialsId: 'NexusAdmin', groupId: 'SpecOps', nexusUrl: 'ec2-52-30-48-198.eu-west-1.compute.amazonaws.com:50000/nexus', nexusVersion: 'nexus2', protocol: 'http', repository: 'releases', version: env.BUILD_NUMBER
     } 
 }
